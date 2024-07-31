bdbdfd5fdd2f21ae7f7cdcdd579cf6f1f0fd6c06
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-31 18:05:31 +0200

    fix: removed unwanted testing button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,6 @@ export default function Home() {
           height={37}
           priority
         />
-        <Button>Shadcn Button</Button>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
--- src/components/forms/basic-form.tsx
+++ src/components/forms/basic-form.tsx
@@ -64,7 +64,7 @@ export const BasicForm = ({ form, onSubmit }: BasicFormProps) => {
                   </SelectTrigger>
                 </FormControl>
                 <SelectContent>
-                  <SelectItem value="AF">Afghanistan</SelectItem>
+                  <SelectItem value="GB">Great Britain</SelectItem>
                   <SelectItem value="FR">France</SelectItem>
                   <SelectItem value="US">United States</SelectItem>
                 </SelectContent>
